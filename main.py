6d7f1eb0f6920598bf467c587dbddd56b5cfebec
Austin Warholic <awarholic24@biomedscienceacademy.org>
2020-11-05 16:33:21 +0000

    made inputs

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
+Author: Austin Warholic
+Period/Core:2
 
 """
+
+input1 = input("Input an adjective:") 
+input2 = input("Input an adjective:")
+input3 = input("Input a noun:")
+input4 = input("Input a verb:")
+input5 = input("Input an adjective:")
+input6 = input("Input a verb:")
+input7 = input("Input a number:")
+input8 = input("Input a monster:")
+input9 = input("Input a number:")
