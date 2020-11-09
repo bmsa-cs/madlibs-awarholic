8b692670168536e6193c3ca7afa359d522aa37d2
Austin Warholic <awarholic24@biomedscienceacademy.org>
2020-11-09 15:27:57 +0000

    added title

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Period/Core:2
 
 """
 
+input0 = input("input a name:")
 input1 = input("Input an adjective:") 
 input2 = input("Input an adjective:")
 input3 = input("Input a noun:")
@@ -14,5 +15,8 @@ input6 = input("Input a verb:")
 input7 = input("Input a number:")
 input8 = input("Input a monster:")
 input9 = input("Input a number:")
+
+title = "The story of {fname}!".format(fname = input0)
+print(title)
 
 print("one "+input1+" Halloween night a swarm of kids flew from \n house to house grabbing candy. As they ran down the street they \n saw the "+input2+" house on the block "+input3+" manor! They \n"+input4+" up to the porch of "+input3+" manor they had a \n"+input5+ "bowl of candy. They "+input6+" their hands through the \n candy searching for their favorites. The sign said grab one but they \n grabbed "+input7+" and ran off in their silly "+input8+" \n costumes. They headed back down the street picking off the "+input9+" \n houses left on their list.")
